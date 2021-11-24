d301e50ac617b6052ddd7a10fd4d8386aa7ad836
Пользователь <alena_kovganko@mail.ru>
2021-11-24 11:04:07 +0300

    02

---
+++ Task02/Program.cs
@@ -0,0 +1,45 @@
+﻿// Дан целочисленный двумерный массив, размерности n х m. Найти сумму и произведение четных элементов.
+int [,] a = new int [2,3];
+void Array(int [,] a)
+{
+    for(int i =0; i<a.GetLength(0); i++)
+    {
+        for(int j=0; j<a.GetLength(1);j++)
+        {
+            a[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int [,] a)
+{
+    for(int i=0; i < a.GetLength(0); i++)
+    {
+        for(int j=0; j <a.GetLength(1);j++)
+        {
+            Console.Write($"{a[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+void EvenSumMulti(int [,] a, out int sum, out int multi)
+{
+    sum = 0;
+    multi = 1;
+    for(int i =0; i<a.GetLength(0); i++)
+    {
+        for(int j=0; j<a.GetLength(1);j++)
+        {
+            if(a[i,j]%2==0) sum = sum + a [i,j]; 
+            if(a[i,j]%2==0) multi = multi* a[i,j]; 
+        }
+
+    }
+
+}
+Array(a);
+Console.WriteLine("Рандомный массив");
+PrintArray(a);
+int m,s;
+EvenSumMulti(a, out s, out m);
+Console.WriteLine($"Сумма всех четных элементов {s}");
+Console.WriteLine($"Произведение всех четных элементов {m}");
