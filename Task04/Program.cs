1edcce39bf2ad98d75a5f3704873110240ebeb96
Пользователь <alena_kovganko@mail.ru>
2021-11-24 16:26:30 +0300

    04

---
+++ Task04/Program.cs
@@ -0,0 +1,42 @@
+﻿// Дан целочисленный двумерный массив, размерности n х m. Найти количество отрицательных элементов, больше -9.
+int [,] a = new int [3,4];
+void Array(int [,] a)
+{
+    for(int i =0; i <a.GetLength(0); i++)
+    {
+        for(int j = 0; j<a.GetLength(1); j++)
+        {
+            a [i,j]  = new Random().Next(-45,45);
+        }
+    }
+}
+void PrintArray(int [,] a)
+{
+    for(int i=0; i<a.GetLength(0); i++)
+    {
+        for(int j = 0;j<a.GetLength(1); j++)
+        {
+            Console.Write($"{a[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+void Count(int [,] a, out int count)
+{
+    count = 0;
+    for(int i=0; i<a.GetLength(0); i++)
+    {
+        for(int j = 0;j<a.GetLength(1); j++)
+        {
+            if(a[i,j]>-9 & a[i,j]<0) count = count + 1; 
+        }
+    }
+    
+}
+
+Array(a);
+PrintArray(a);
+int c;
+Count(a, out c);
+Console.WriteLine($"Количество отрицательных элементов, больше -9. = {c}");
+
